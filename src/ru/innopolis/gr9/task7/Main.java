3d156ccc969644aa24b38f3fa7bdb429d76e485e
AVIvanchenko-STC <a.ivanchenko@innopolis.ru>
2018-05-01 12:39:08 +0300

    import task 7

---
+++ src/ru/innopolis/gr9/task7/Main.java
@@ -0,0 +1,54 @@
+package ru.innopolis.gr9.task7;
+
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+
+        LoadListFile loadListFile = new LoadListFile();
+
+        List<String > listFile = new ArrayList<>();
+
+        //скачасть файлы
+        //loadListFile.downloadFiles((ArrayList<String>) listFile);
+        //Создать большой файл
+        // loadListFile.createBigFile(1200000, (ArrayList<String>) listFile);
+
+        //Добавить файлы из локальной папки file
+        listFile.addAll(loadListFile.loadListInFile(100000));
+
+        //Добавить ссылки на файлы в интернете
+        //listFile.addAll(loadListFile.loadListData(100,"http://tululu.org/sitemap-file-3.xml"));
+
+        //Добавить большой файл в середину списка
+        //listFile.set(listFile.size()/2,"big.txt");
+
+        //Добавить большой файл в коней списка
+        //listFile.add("big.txt");
+
+        //Сортировка локальных файлов по убыванию размер файла. онлайн файлы помещаются в конец списка
+        listFile =  loadListFile.sortListInFile(listFile);
+
+        String[] sources = listFile.toArray(new String[0]);
+
+        //Список поисковых слов
+        String[] words = new String[]{"картина","подушка","собака","шишка","земля","любовь","секунд","время","жизнь","красота",
+        "мыло","еда","самолет","доктор","сила","бег","велосипед","красота","компьютер","окно",
+        "шкаф","шарф","жираф","лошадь","фотография","ложь","дорога","дерево","зверь","корабль"};
+
+
+        Occurencies occurencies = new Occurencies(8);
+        occurencies.getOccurencies(sources,words,"asd.txt");
+
+    }
+
+
+}
---
+++ src/ru/innopolis/gr9/task7/Occurencies.java
@@ -0,0 +1,60 @@
+package ru.innopolis.gr9.task7;
+
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class Occurencies{
+
+    final static Logger logger = Logger.getLogger(Occurencies.class);
+
+    private int countThread = 1;
+
+    public Occurencies(int countThread) {
+        this.countThread = countThread;
+    }
+
+    public void getOccurencies(String[] sources, String[] words, String res) throws ExecutionException, InterruptedException, IOException {
+
+        logger.info("Start scannig");
+
+        //Создание потока для записи в файл
+        WriteThread thread = new WriteThread(res);
+        thread.start();
+
+        //Создание пула потоков для чтения файлов
+        ExecutorService threadPool = Executors.newFixedThreadPool(countThread);
+        List<Future<Boolean>> futures = new ArrayList<>();
+        int i = 0;
+        ScanFile scanFile = new ScanFile(thread);
+        for(String path:sources){
+            final String pathF = path;
+            final int j = i++;
+            final String[] wordsF = words;
+            futures.add(
+                    CompletableFuture.supplyAsync(
+                            () -> {
+                                try {
+                                    return scanFile.scanFile(pathF, wordsF, j);
+                                } catch (IOException e) {
+                                    return false;
+                                }
+                            },
+                            threadPool
+                    ));
+
+        }
+
+        for (Future<Boolean> future : futures) {
+            future.get();
+        }
+
+        thread.stopWriter();
+        logger.info("Stop scannig");
+    }
+
+
+}
